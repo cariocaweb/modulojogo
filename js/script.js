2ff611ddf2471619b11e767eb34dc25d45c48c31
Jose Duarte <contato@cariocaweb.com.br>
2020-03-28 18:40:46 -0300

    Gerando bolar coloridas e placar

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ function addBola(){
     var p1 = Math.floor(Math.random() * 500); 
     // eixo y
     var p2 = Math.floor(Math.random() * 400); 
+    //atribur nova cor a bola
+    var cor = Math.floor(Math.random() * 909099);
     //localização da bola
-    bola.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;");   
+    bola.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;background-color:#"+ cor +";");   
     // ação de clicar na bola 
     bola.setAttribute("onclick", "estourar(this)"); 
     //colocar na tela
@@ -14,6 +16,13 @@ function addBola(){
 }
 function estourar(elemento){
     document.body.removeChild(elemento);
+    var p = parseInt(document.getElementById("pontos").innerHTML);
+    p = p + 1;
+    document.getElementById("pontos").innerHTML = p;
+    if (p > 50){
+        alert ("uhuuu Ganhou");
+        window.location.href=window.location.href;
+    }
 }
 function iniciar(){
     setInterval(addBola, 1000);
