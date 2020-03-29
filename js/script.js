b61145755a13af5af4785a5c33904c10d5ab7cd4
Jose Duarte <contato@cariocaweb.com.br>
2020-03-28 21:50:09 -0300

    melhorias no efeito e botao de inicio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+function clicou(){
+    onload=iniciar("clicou");
+}
+function iniciar(){
+    setInterval(addBola, 1000);
+}
 function addBola(){
     var bola = document.createElement("div");
     bola.setAttribute("class", "bola");
@@ -6,24 +12,22 @@ function addBola(){
     // eixo y
     var p2 = Math.floor(Math.random() * 400); 
     //atribur nova cor a bola
-    var cor = Math.floor(Math.random() * 909099);
+    var cor = Math.floor(Math.random() * 999999);
     //localização da bola
     bola.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;background-color:#"+ cor +";");   
     // ação de clicar na bola 
     bola.setAttribute("onclick", "estourar(this)"); 
     //colocar na tela
-    document.body.appendChild(bola);
+    /*document.body.appendChild(bola);*/
+    document.body.appendChild(bola, arena);
 }
 function estourar(elemento){
     document.body.removeChild(elemento);
     var p = parseInt(document.getElementById("pontos").innerHTML);
     p = p + 1;
     document.getElementById("pontos").innerHTML = p;
-    if (p > 50){
+    if (p > 20){
         alert ("uhuuu Ganhou");
         window.location.href=window.location.href;
     }
-}
-function iniciar(){
-    setInterval(addBola, 1000);
 }
